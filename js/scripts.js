f50ce10b45745e6df1385d3ee9d3d54ea2b27435
TK <tk@tk.pl>
2017-07-17 16:50:26 +0200

    ok

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+﻿var getButtons = document.getElementsByClassName('button');
+console.log(getButtons);
+console.log(getButtons.length);
+
+for (i=0; i<getButtons.length; i++) {
+	console.log(getButtons[i].innerText);
+}
